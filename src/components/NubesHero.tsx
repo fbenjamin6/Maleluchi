fd52c046b86d3d4c5f8a51f4cf8d259648ac0ec0
fbenjamin <benjamin.federico6@gmail.com>
2026-04-04 13:20:28 -0300

    responsive casi hecho, falta ocmodar imagenes

--- src/components/NubesHero.tsx
+++ src/components/NubesHero.tsx
@@ -4,23 +4,42 @@ export function NubesHero() {
   return (
     <div className=' h-[90dvh] relative z-10 overflow-hidden'>
       <ParallaxLayer style={{ top: '0%', zIndex: 1 }} speed={0.01} offset={0}>
-        <img src='./src/assets/nubesHero5.svg' alt='' className='w-full' />
+        <img
+          src='./src/assets/nubesHero5.svg'
+          alt=''
+          className='w-full max-w-none'
+          style={{ width: '200%' }}
+        />
       </ParallaxLayer>
       <ParallaxLayer style={{ top: '10%', zIndex: 2 }} speed={0.15} offset={0}>
-        <img src='./src/assets/nubesHero4.svg' alt='' className='w-full' />
+        <img
+          src='./src/assets/nubesHero4.svg'
+          alt=''
+          className='w-full max-w-none'
+          style={{ width: '200%' }}
+        />
       </ParallaxLayer>
       <ParallaxLayer style={{ top: '30%', zIndex: 3 }} speed={0.25} offset={0}>
         <img
           src='./src/assets/nubesHero3.svg'
           alt=''
-          className='w-full'
-          style={{ zIndex: 3 }}
+          className='w-full max-w-none'
+          style={{ zIndex: 3, width: '200%' }}
         />
       </ParallaxLayer>
       <ParallaxLayer style={{ top: '50%', zIndex: 4 }} speed={0.35} offset={0}>
-        <img src='./src/assets/nubesHero2.svg' alt='' className='w-full' />
+        <img
+          src='./src/assets/nubesHero2.svg'
+          alt=''
+          className='w-full max-w-none'
+          style={{ width: '200%' }}
+        />
       </ParallaxLayer>
-      <ParallaxLayer style={{ top: '70%', zIndex: 5 }} speed={0.4} offset={0}>
+      <ParallaxLayer
+        style={{ top: '70%', zIndex: 5, width: '200%' }}
+        speed={0.4}
+        offset={0}
+      >
         <img src='./src/assets/nubesHero1.svg' alt='' className='w-full' />
       </ParallaxLayer>
     </div>
